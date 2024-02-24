9457a767c8c2662ceede7f1dca739bd9e3fbbeae
BIBIN <bibinkp749@gmail.com>
2024-02-24 15:58:20 +0530

    started creating BlogSite

---
+++ BlogSite/src/Pages/CreateBlogs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateBlogs = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CreateBlogs
---
+++ BlogSite/src/Pages/IndividualBlocks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IndividualBlocks = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default IndividualBlocks
